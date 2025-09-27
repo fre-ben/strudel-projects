273bec54d52c74fe2b4a9edf8d40cdc9ca51c543
Frederik <77615388+fre-ben@users.noreply.github.com>
2025-09-28 01:07:42 +0200

    Update random-sample.js

--- random-sample.js
+++ random-sample.js
@@ -4,10 +4,16 @@ samples('github:ash3rw/strudel-samples')
 
 setcpm(130/4)
 
+const voxPatterns = [
+  "<49 77 99 104 72>*4",
+  "<49 62 99 101 72>*4",
+  "<42 77 201 104 72>*4",
+   ]
+
 VOX$: s("vox").fit()
   .note("<a-1@5 e-1@3>*8")
   .scrub(irand(16).div(16).seg(8)
-  .rib("<49 22 77 99 104 72>*4", 2)
+  .rib(pick(voxPatterns, "<0 1 0 2 >"), 2)
   )
   .slow(2)
   .almostNever(x => x.ply("2 | 4"))
@@ -15,8 +21,9 @@ VOX$: s("vox").fit()
   .room(1.5).roomsize(2)
   .distort("3:0.26")
   .hpf(500)
-  .postgain(.8)
   .orbit(2)
+  // .jux(rev)
+  .postgain(.8)
   .color("hotpink")._scope()
 
 SIDECHAIN$: s("bd:2!2").bank("RolandTR909")
@@ -25,22 +32,26 @@ SIDECHAIN$: s("bd:2!2").bank("RolandTR909")
 
 const KICK_PLAYING = 1;
 
-KICK$: s("bd:1!2").bank("RolandTR909")
-  .sometimesBy("0.21", x => x.ply("2 | 4"))
+KICK$: stack(
+  s("bd:1!2").bank("RolandTR909")
+  .sometimesBy("0.21", x => x.ply("2 | 4 "))
   .speed(0.8)
   .room(1.4).roomsize(1.2)
   .distort("4:0.35")
-  .postgain(KICK_PLAYING ? 1 : 0)
-  .color("darkblue")._punchcard()
-KICK2$: s("bd:0!2").bank("RolandTR909")
+  .color("darkblue")._punchcard(),
+  s("bd:0!2").bank("RolandTR909")
   .sometimesBy("0.21", x => x.ply("2 | 4"))
   .room(1.4).roomsize(1.3)
   .distort("4:0.35")
   .lpf(300)
-  .postgain(KICK_PLAYING ? .6 : 0)
+  .postgain(.6)
   .color("darkblue")._punchcard()
+)
+  // .crush(saw2.range(0,4).fast(2))
+  .postgain(KICK_PLAYING ? 1 : 0)
+
   
-SNARE$: s("[- cp:0]*2").bank("RolandTR909")
+SNARE$: s("[- cp]*2").bank("RolandTR909")
   .sometimesBy("0.22", x => x.ply("2 | 4"))
   .speed("0.9 | 0.78 | 0.86")
   .room(1.6)
@@ -60,11 +71,8 @@ HAT$: s("[- hh:4]*4").bank("RolandTR909")
 BASS$: s("sawtooth, supersaw")
   .seg(8)
   .n("<-14@5 -14.5@3>*8")
-  .scale("C:major")
-  .scrub(irand(16).div(16).seg(8)
-  .rib("<.2 57 64>*2", 0.2)
-  )
+  .scale("C:major") 
   .lpf(slider(200, 200, 1500, 100))
   .distort("2:0.7")
   .orbit(2)
-  .postgain(.8)
+  .postgain(saw2.range(.2, 1))
