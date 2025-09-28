318291f7dafc2dc6a5178564c3d42a3c9f1f30c1
Frederik <77615388+fre-ben@users.noreply.github.com>
2025-09-28 15:39:52 +0200

    Update random-sample.js

--- random-sample.js
+++ random-sample.js
@@ -1,4 +1,4 @@
-// "random sample Jam" @by gruvt
+// "random sample jam" @by gruvt
 
 samples('github:ash3rw/strudel-samples')
 
@@ -10,7 +10,7 @@ const voxPatterns = [
   "<42 77 201 104 72>*2",
    ]
 
-VOX$: s("vox").fit()
+  _VOX$: s("vox").fit()
   .note("<a-1@5 e-1@3>*8")
   .scrub(irand(16).div(16).seg(8)
   .rib(pick(voxPatterns, "<0 1 0 2 >"), 2)
@@ -23,30 +23,32 @@ VOX$: s("vox").fit()
   .distort("3:0.26 | [6:0.14 3:0.24] | [3:0.22 4:0.12]")
   .hpf(500)
   .orbit(2)
+    // .slow(2)
   .postgain(.8)
   .color("hotpink")._scope()
 
 BASS$: s("sawtooth, supersaw")
   .seg(8)
   .n("<-14@5 -14.5@3>*8")
-   .scale("C:major")
-  // .scale("<C:major C:major C:major, A:major F:major E:major>/2") 
+   // .scale("C:major")
+  .scale("<C:major C:major C:major, A:major F:major E:major>/2") 
   .lpf(slider(200, 200, 2300, 100))
   .distort("2:0.7")
   .orbit(2)
   .jux(rev)
+  // .room(2).roomsize(6
   .postgain(saw2.range(.2, .8))
   .color("chocolate")._scope()
   
 
-SIDECHAIN$: s("bd:2!2").bank("RolandTR909")
+_SIDECHAIN$: s("bd:2!2").bank("RolandTR909")
   .duck("2").duckatt(.4).duckdepth(0.8)
   .postgain(0)
 
 // mute kick + snare
 const KICK_SNARE = 1;
 
-KICK$: stack(
+_KICK$: stack(
   s("bd:1!2").bank("RolandTR909")
   .sometimesBy("0.21", x => x.ply("2 | 4 ").gain(".6 | .78"))
   .speed(0.8)
@@ -63,13 +65,13 @@ KICK$: stack(
 )
   // .crush(saw2.range(0,4).fast(2))
   // .fast(2)
- //  .slow(4) // + slow(2) cp
+  // .slow(4) // + slow(2) cp
   .postgain(KICK_SNARE ? 1 : 0)
   .note("[c2 cb2 cb2 c2]/2")
   ._punchcard()
 
   
-SNARE$: s("[- cp]*2").bank("RolandTR909")
+_SNARE$: s("[- cp]*2").bank("RolandTR909")
   .sometimesBy("0.22", x => x.ply("2 | 4").gain(".85 | .79"))
   .speed("0.9 | 0.78 | 0.86")
   .room(1.6)
@@ -78,7 +80,7 @@ SNARE$: s("[- cp]*2").bank("RolandTR909")
  // .slow(2) // + slow(4) bd
   .color("salmon")._punchcard()
 
-HAT$: s("[- hh:4]*4").bank("RolandTR909")
+_HAT$: s("[- hh:4]*4").bank("RolandTR909")
   .sometimesBy(".7", x => x.ply("2").gain(".55 | .64"))
   .sometimesBy(".2", x => x.ply("4").gain(".4 | .5"))
   .delay(0.7)
